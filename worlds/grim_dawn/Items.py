f6f651a5efc28bcc1316613f8e9f1c943ad8d330
Faris <faristheancient@gmail.com>
2023-12-17 12:52:55 -0600

    New locations and items
    still need rules

--- worlds/grim_dawn/Items.py
+++ worlds/grim_dawn/Items.py
@@ -16,68 +16,20 @@ class GrimDawnItemData(NamedTuple):
 
 
 item_data_table: Dict[str, GrimDawnItemData] = {
-    "Cultist Orders": GrimDawnItemData(
-        code=baseId,
-        type=ItemClassification.progression,
-    ),
-    "Strange Key": GrimDawnItemData(
-        code=baseId+1,
-        type=ItemClassification.progression,
-    ),
-    "Dynamite": GrimDawnItemData(
-        code=baseId+2,
-        type=ItemClassification.useful,
-        quantity=2
-    ),
-    "Scrap": GrimDawnItemData(
-        code=baseId+3,
-        type=ItemClassification.progression,
-        quantity=5
-    ),
-    "Flooded Passage Destroy Blockade": GrimDawnItemData(
-        code=baseId+4,
-        type=ItemClassification.progression,
-    ),
-    "Lower Crossing Destroy Blockade": GrimDawnItemData(
-        code=baseId+5,
-        type=ItemClassification.progression,
-    ),
-    "East Marsh Bridge Repair": GrimDawnItemData(
-        code=baseId+6,
-        type=ItemClassification.progression,
-    ),
-    "Arkovia Bridge Repair": GrimDawnItemData(
-        code=baseId+7,
-        type=ItemClassification.progression,
-    ),
-    "Warden's Cellar Door": GrimDawnItemData(
-        code=baseId+8,
-        type=ItemClassification.progression,
-    ),
-    "Wightmire Bridge Repair": GrimDawnItemData(
-        code=baseId+9,
-        type=ItemClassification.useful
-    ),
-    "Level Up": GrimDawnItemData(
-        code=baseId+10,
-        type=ItemClassification.useful,
-        quantity=2
-    ),
-    "Skill Points": GrimDawnItemData(
-        code=baseId+11,
-        type=ItemClassification.useful,
-        quantity=2
-    ),
-    "Aether Crystals": GrimDawnItemData(
-        code=baseId+12,
-        type=ItemClassification.filler,
-        quantity=2
-    ),
-    "Extra EXP": GrimDawnItemData(
-        code=baseId+13,
-        type=ItemClassification.filler,
-        quantity=2
-    ),
+    "Cultist Orders": GrimDawnItemData( code=baseId, type=ItemClassification.progression, ),
+    "Strange Key": GrimDawnItemData( code=baseId+1, type=ItemClassification.progression, ),
+    "Dynamite": GrimDawnItemData( code=baseId+2, type=ItemClassification.useful, quantity=2 ),
+    "Scrap": GrimDawnItemData( code=baseId+3, type=ItemClassification.progression, quantity=5 ),
+    "Flooded Passage Destroy Blockade": GrimDawnItemData( code=baseId+4, type=ItemClassification.progression, ),
+    "Lower Crossing Destroy Blockade": GrimDawnItemData( code=baseId+5, type=ItemClassification.progression, ),
+    "East Marsh Bridge Repair": GrimDawnItemData( code=baseId+6, type=ItemClassification.progression, ),
+    "Arkovia Bridge Repair": GrimDawnItemData( code=baseId+7, type=ItemClassification.progression, ),
+    "Warden's Cellar Door": GrimDawnItemData( code=baseId+8, type=ItemClassification.progression, ),
+    "Wightmire Bridge Repair": GrimDawnItemData( code=baseId+9, type=ItemClassification.useful ),
+    "Level Up": GrimDawnItemData( code=baseId+10, type=ItemClassification.useful, quantity=2 ),
+    "Skill Points": GrimDawnItemData( code=baseId+11, type=ItemClassification.useful, quantity=2 ),
+    "Aether Crystals": GrimDawnItemData( code=baseId+12, type=ItemClassification.filler, quantity=2 ),
+    "Extra EXP": GrimDawnItemData( code=baseId+13, type=ItemClassification.filler, quantity=2 ),
 
 }
 
