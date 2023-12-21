5a468784b15c6273ddce522a1b1eae1e02d7ac8f
Faris <faristheancient@gmail.com>
2023-12-20 22:13:35 -0600

    Forgot to add options lol

--- worlds/grim_dawn/Options.py
+++ worlds/grim_dawn/Options.py
@@ -3,16 +3,46 @@ from typing import Dict
 from Options import Choice, Option, Toggle, PerGameCommonOptions
 from dataclasses import dataclass
 
+class GrimDawnGoal(Choice):
+    """
+    The goal to accomplish in order to complete the seed.
+    Beat Warden - Finish Act 1 and kill the Warden Krieg
+    Beat Ravna - Finish Act 3 and kill Swarm Queen Ravna
+    Beat Karroz - Finish Act 4 and kill Karroz, Sigil of Ch'thon
+    """
+    display_name = "Goal"
 
-class DevotionShrines(Toggle):
+    option_beat_warden = 0
+    option_beat_ravna = 1
+    option_beat_karroz = 2
+
+    default = 0
+
+class GrimDawnForbiddenDungeons(Toggle):
+    """Enable Forbidden Dungeons and Skeleton Keys as locations"""
+    display_name="Forbidden Dungeons and Skeleton Keys"
+
+class GrimDawnOneShot(Toggle):
+    """Enable One Shot Chests as locations"""
+    display_name="One Shot Chests"
+
+class GrimDawnSecretChest(Toggle):
+    """Enable Secret Chests as locations"""
+    display_name="Secret Chests"
+
+class GrimDawnDevotionShrines(Toggle):
     """Enable Devotion Shrines as locations"""
     display_name="Devotion Shrines"
 
-class Lore(Toggle):
+class GrimDawnLore(Toggle):
     """Enable lore locations"""
     display_name="Lore"
 
 @dataclass
 class GrimDawnOptions(PerGameCommonOptions):
-    devotion_shrines: DevotionShrines
-    lore: Lore
+    goal: GrimDawnGoal
+    forbidden_dungeons: GrimDawnForbiddenDungeons
+    one_shot: GrimDawnOneShot
+    secret_chest: GrimDawnSecretChest
+    devotion_shrines: GrimDawnDevotionShrines
+    lore: GrimDawnLore
