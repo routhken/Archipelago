7293e14f8cbc232e2bff1b748d056bce2172ac03
Faris <faristheancient@gmail.com>
2024-02-29 18:27:16 -0600

    nvmd version is actually 0.1.1

--- worlds/grim_dawn/__init__.py
+++ worlds/grim_dawn/__init__.py
@@ -8,7 +8,7 @@ from .Options import GrimDawnOptions
 from .Regions import region_data_table
 from .Rules import GrimDawnRules
 
-#release version 1.0.1
+#release version 0.1.1
 
 class GrimDawnWebWorld(WebWorld):
     theme = "partyTime"
