78fded4ed62a6d429098cb7872d6f0f43a7a1935
Faris <faristheancient@gmail.com>
2024-06-29 13:08:49 -0500

    Final update for Grim Dawn v0.1.4

--- worlds/grim_dawn/Rules.py
+++ worlds/grim_dawn/Rules.py
@@ -49,6 +49,8 @@ class GrimDawnRules:
                 state.has("Homestead Main Doors Unlock",self.player),
             "Act 3 -> Steps of Torment": lambda state:
                 state.has("Forbidden Door Unlock", self.player),
+            "Act 3 -> Act 7": lambda state:
+                state.has("Gloomwald Destroy Blockade",self.player),
             
             "Homestead Side Doors -> Royal Hive": lambda state:
                 state.has("Royal Hive Queen Door Unlock",self.player),
@@ -70,8 +72,6 @@ class GrimDawnRules:
             "Tomb of the Watchers -> Edge of Madness": lambda state:
                 state.has("Loghorrean Seal Unlock",self.player),
             
-            "Act 5 -> Act 7": lambda state:
-                state.has("Gloomwald Destroy Blockade",self.player),
             
             "Act 7 -> Nane's Hideout": lambda state:
                 state.has("Nane's Hideout Destroy Barricade",self.player),
@@ -84,7 +84,7 @@ class GrimDawnRules:
             "Act 7 -> Act 8": lambda state:
                 state.has("Altar of Rattosh Portal",self.player),
             "Act 8 -> Candle Disctict": lambda state:
-                state.has_any(["Candle District Door Unlock","Malmouth Bridge Lowered"],self.player),
+                state.has("Candle District Door Unlock",self.player),
             "Act 8 -> Act 9": lambda state:
                 state.has("Steelcap District Door Unlock",self.player),
             
@@ -113,10 +113,10 @@ class GrimDawnRules:
             "Find Elsa":                                        lambda state: self.has_scrap(state,5) and state.has("Warden Boss Door Unlock",self.player),
             "Trapped and Alone":                                lambda state: state.has("Rotting Croplands Destroy South Blockade",self.player),
             "Rashalga, the Mad Queen":                          lambda state: state.has("Lower Crossing Destroy Blockade",self.player),
-            "The Hidden Path":                                  lambda state: state.has_all(["Lower Crossing Destroy Blockade","Arkovia Bridge Repair","Arkovian Foothills Destroy Barricade","Homestead Main Doors Unlock","Homestead Side Doors Unlock"],self.player),
-            "Rescue Garett Torvan":                             lambda state: state.has_all(["Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock"],self.player),
-            "Convince Ulgrim":                                  lambda state: state.has_all(["Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock"],self.player),
-            "Steelcap District Riftgate Secured":               lambda state: state.has_all(["Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock"],self.player),
+            "The Hidden Path":                                  lambda state: state.has_all(["Lower Crossing Destroy Blockade","Arkovia Bridge Repair","Arkovian Foothills Destroy Barricade","Homestead Main Doors Unlock"],self.player),
+            "Rescue Garett Torvan":                             lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Convince Ulgrim":                                  lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Steelcap District Riftgate Secured":               lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
             "Depraved Sanctuary Exalted Stash":                 lambda state: state.has("Strange Key", self.player),
             "Rotting Croplands Exalted Stash":                  lambda state: state.has("Rotting Croplands Destroy North Blockade",self.player),
             "Old Grove Secret Chest":                           lambda state: state.has("Old Grove Bridge Repair",self.player),
@@ -124,23 +124,44 @@ class GrimDawnRules:
             "The Hidden Path - Solael":                         lambda state: state.has("Lower Crossing Destroy Blockade",self.player),
             "The Hidden Path - Bysmiel":                        lambda state: state.has("Lower Crossing Destroy Blockade",self.player),
             "Journal of Inquisitor Creed - 9th Entry":          lambda state: state.has("Warden Boss Door Unlock",self.player),
-            "Clippings from Ivonda's Memory":                   lambda state: state.has_all(["Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock"],self.player),
+            "Clippings from Ivonda's Memory":                   lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Onarix, the Razorhide":                            lambda state: state.has("Ruins of Abyd Open Portal",self.player),
             "Inarah, the Afflicted":                            lambda state: state.has("Tomb of Nephos Destroy Barrier",self.player),
+            "Lost Tomb of the Damned Secret Chest":             lambda state: state.has("Ruins of Abyd Open Portal",self.player),
             "Guardian of Dreeg":                                lambda state: state.has("Lower Crossing Destroy Blockade",self.player),
             "Salazar, Blade of Ch'thon":                        lambda state: state.has("Strange Key",self.player),
             "Dangerous Curiosity":                              lambda state: state.has("Devil's Crossing Revered",self.player),
             "Ellena, the First Slith":                          lambda state: state.has("Devil's Crossing Revered",self.player),
+            "Guardian of Solael":                               lambda state: state.has("Lower Crossing Destroy Blockade",self.player),
+            "Making a Deal":                                    lambda state: state.has_all(["Homestead Revered","Warden Boss Door Unlock"],self.player),
             "Death's Vigil - Kymon's Chosen Faction Quest 5":   lambda state: state.has("Death's Vigil - Kymon's Chosen Revered",self.player),
             "Death's Vigil - Kymon's Chosen Faction Quest 6":   lambda state: state.has("Death's Vigil - Kymon's Chosen Revered",self.player),
             "Guardian of Bysmiel":                              lambda state: state.has("Lower Crossing Destroy Blockade",self.player),
-            "Rover Legacy":                                     lambda state: state.has("Rovers Revered",self.player),
+            "Rhowari Legacy":                                   lambda state: state.has("Rovers Revered",self.player),
             "Herald of Destruction":                            lambda state: state.has("Black Legion Revered",self.player),
-            "Power in the Circles":                             lambda state: state.has("Coven of Ugdenbog Revered",self.player),
-            "The Final Seal":                                   lambda state: state.has("Coven of Ugdenbog Revered",self.player),
+            "The Coven's Walls":                                lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Pruning The Weeds":                                lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Allure of the Wendigo":                            lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Trapped Wraith 1":                                 lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Trapped Wraith 2":                                 lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Trapped Wraith 3":                                 lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Power in the Circles":                             lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Stone Basilisk":                                   lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Final Seal":                                       lambda state: state.has_all(["Coven of Ugdenbog Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
             "Galakros, the Mountain":                           lambda state: state.has("Malmouth Sewers Destroy Blockade",self.player),
-            "Alberran Rein":                                    lambda state: state.has("Malmouth Resistance Revered",self.player),
-            "Dreven Cole":                                      lambda state: state.has("Malmouth Resistance Revered",self.player),
-            "Cyrian Marcan":                                    lambda state: state.has("Malmouth Resistance Revered",self.player),
+            "Someone on the Inside":                            lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Bring out Your Dead":                              lambda state: state.has_all(["Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Secure the Blackiron Docks":                       lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Rise of the Insurgence":                           lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Alberran Rein":                                    lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Dreven Cole":                                      lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Cyrian Marcan":                                    lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Chamber of the High Council Secret Chest":         lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Hargate's Journal - Page 2":                       lambda state: state.has_all(["Arkovia Bridge Repair","Devil's Crossing Revered"],self.player),
+            "Hargate's Journal - Page 3":                       lambda state: state.has_all(["Arkovia Bridge Repair","Devil's Crossing Revered"],self.player),
+            "Hargate's Journal - Page 4":                       lambda state: state.has_all(["Arkovia Bridge Repair","Devil's Crossing Revered"],self.player),
+            "Nearan's Work Log":                                lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
+            "Council Report to Theodin Marcell":                lambda state: state.has_all(["Malmouth Resistance Revered","Homestead Main Doors Unlock","Fort Ikon Gate Unlock","Fort Ikon Destroy Blockade","Tomb of the Watchers Door Unlock","Loghorrean Seal Unlock","Homestead Side Doors Unlock"],self.player),
 
         }
 
